2982b9e3d6890ebbca7f5e9c7e68e39052e05089
Luis <lfmonten@espol.edu.ec>
2021-07-01 11:31:24 -0500

    server.js agregado

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static('./dist/project'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: './dist/project/' }
+    );
+});
+app.listen(process.env.PORT || 8080);
